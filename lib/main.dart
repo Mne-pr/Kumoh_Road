963acb544bbc7f245fd54b92ff5f8f5e9e7809cf
Jeyong <joon6093@naver.com>
2023-11-08 05:37:58 +0900

    [ADD] 앱 가이드 및 로그인 UI 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:kumoh_road/samples/kakao_login.dart';
 import 'package:kumoh_road/samples/main_view_model.dart';
 import 'firebase_options.dart';
@@ -17,63 +18,142 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Kumoh Road',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const IntroScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _IntroScreenState createState() => _IntroScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final viewModel = MainViewModel(KakaoLogin());
+class _IntroScreenState extends State<IntroScreen> {
+  final PageController _pageController = PageController();
+  final List<Widget> _pages = [
+    GuidePage(
+      icon: Icons.local_taxi,
+      title: '택시 요금을 줄여보세요',
+      description: '공유하면 학생들이 모여 택시비를 줄일 수 있어요!',
+    ),
+    GuidePage(
+      icon: Icons.directions_bus,
+      title: '정확한 버스 정보를 받아보세요',
+      description: '실시간 버스 정보를 이용하세요!',
+    ),
+    GuidePage(
+      icon: Icons.directions_bike,
+      title: '경로정보를 이용하세요',
+      description: '구미 대중교통에 스트레스 받지마시고 마음편하게 학교로 가세요',
+    ),
+    KakaoLoginPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // 기존 이미지 표시 조건
-            if (viewModel.user?.kakaoAccount?.profile?.profileImageUrl != null
-                && viewModel.user!.kakaoAccount!.profile!.profileImageUrl!.isNotEmpty)
-              Image.network(viewModel.user!.kakaoAccount!.profile!.profileImageUrl!),
-
-            Text(
-              '${viewModel.isLogined}',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Stack(
+        children: [
+          PageView(
+            controller: _pageController,
+            children: _pages,
+          ),
+          Positioned(
+            left: 16,
+            right: 16,
+            bottom: 16,
+            child: SmoothPageIndicator(
+              controller: _pageController,
+              count: _pages.length,
+              effect: WormEffect(),
+              onDotClicked: (index) => _pageController.animateToPage(
+                index,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeInOut,
+              ),
             ),
-
-            // 카카오 로그인 이미지 버튼
-            InkWell(
-              onTap: () async {
-                await viewModel.login();
-                setState(() {});
-              },
-              child: Image.asset('assets/images/kakao_login_medium_wide.png'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
 
+class GuidePage extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String description;
+
+  const GuidePage({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.description,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, size: 100),
+        const SizedBox(height: 24),
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          description,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 16,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class KakaoLoginPage extends StatelessWidget {
+  final viewModel = MainViewModel(KakaoLogin());
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/images/app_logo.png', width: 100, height: 100),
+        const SizedBox(height: 24),
+        Text(
+          '카카오 계정으로 로그인',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          '간편하게 로그인하고 서비스를 이용하세요!',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16),
+        ),
+        const SizedBox(height: 24),
+        InkWell(
+          onTap: () async {
+            await viewModel.login();
+          },
+          child: Image.asset('assets/images/kakao_login_medium_wide.png'),
+        ),
+      ],
+    );
+  }
+}
