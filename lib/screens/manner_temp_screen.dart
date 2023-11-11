1c12fb24e8b5725913bc5b1675695fc4b2e3925e
Jeyong <joon6093@naver.com>
2023-11-12 05:11:53 +0900

    [ADD] 매너 리뷰 기능 구현 및 매너 상세 화면 구현

---
+++ lib/screens/manner_temp_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/kakao_login_providers.dart';
+import '../widgets/manner_detail_widget.dart';
+
+class MannerTemperatureScreen extends StatelessWidget {
+  MannerTemperatureScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = Provider.of<KakaoLoginProvider>(context);
+    final mannerList = _getFilteredMannerList(userProvider.mannerList);
+    final unmannerlyList = _getFilteredMannerList(userProvider.unmannerList);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('매너 상세', style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.black),
+        elevation: 0,
+        titleSpacing: -5.0,
+      ),
+      body: MannerDetailsWidget(
+        mannerList: mannerList,
+        unmannerlyList: unmannerlyList,
+      ),
+    );
+  }
+
+  List<Map<String, dynamic>> _getFilteredMannerList(
+      List<Map<String, dynamic>>? manners) {
+    if (manners == null) {
+      return [];
+    }
+    return manners.where((manner) => manner['votes'] > 0).toList();
+  }
+}
