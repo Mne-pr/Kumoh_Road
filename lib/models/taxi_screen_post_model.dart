a3656bcca2b48073088479763abb034701b5c195
thk <qkdrhks123@gmail.com>
2023-11-20 18:35:45 +0900

    [UPDATE] User, Post 읽어오는 함수 추가

--- lib/models/taxi_screen_post_model.dart
+++ lib/models/taxi_screen_post_model.dart
@@ -1,10 +1,11 @@
-class TaxiScreenPostModel{
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class TaxiScreenPostModel {
   final String writerId;
   final String title;
   final String content;
   final DateTime createdTime;
   final int viewCount;
-  final List<dynamic> commentsList;
   final String imageUrl;
   final List<dynamic> membersIdList;
 
@@ -14,8 +15,24 @@ class TaxiScreenPostModel{
     required this.content,
     required this.createdTime,
     required this.viewCount,
-    required this.commentsList,
     required this.imageUrl,
     required this.membersIdList,
   });
+
+  static Future<List<TaxiScreenPostModel>> getAllPostsByCollectionName(
+      String collectionName) async {
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection(collectionName).get();
+    List<Map<String, dynamic>> documents = querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+    List<TaxiScreenPostModel> postList = documents.map((doc) => TaxiScreenPostModel(
+            writerId: doc["writer"],
+            title: doc["title"],
+            content: doc["content"],
+            createdTime: doc["createdTime"].toDate(),
+            viewCount: doc["viewCount"],
+            imageUrl: doc["image"],
+            membersIdList: doc["members"]))
+        .toList();
+
+    return postList;
+  }
 }
