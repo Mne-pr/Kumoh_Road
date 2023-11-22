94c62be19e7df18fbc8277f799a1b59350549c34
Mne <42082748+Mne-pr@users.noreply.github.com>
2023-11-22 18:22:22 +0900

    [UPDATE] 버스 목록 가져올 때 로딩화면 발동, 종합버스터미널 추가

--- lib/screens/bus_info_screens/bus_info_screen.dart
+++ lib/screens/bus_info_screens/bus_info_screen.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:kumoh_road/screens/launch_screens/loading_screen.dart';
+import 'package:kumoh_road/screens/bus_info_screens/loading_screen.dart';
 import 'package:kumoh_road/widgets/outline_circle_button.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import '../../models/bus_station_model.dart';
@@ -20,7 +20,11 @@ class BusInfoScreen extends StatefulWidget {
 
 class _BusInfoScreenState extends State<BusInfoScreen> with TickerProviderStateMixin {
 
-  // api 호출
+  // 로딩 상태
+  late bool isLoading = true;
+  late double loadingOpacity = 1.0;
+  
+  // api 호출주소
   final apiAddr = 'http://apis.data.go.kr/1613000/ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList';
   final serKey = 'ZjwvGSfmMbf8POt80DhkPTIG41icas1V0hWkj4cp5RTi1Ruyy2LCU02TN8EJKg0mXS9g2O8B%2BGE6ZLs8VUuo4w%3D%3D';
   
@@ -32,7 +36,8 @@ class _BusInfoScreenState extends State<BusInfoScreen> with TickerProviderStateM
     NMarker(position: NLatLng(36.12963461, 128.3293215), id: "구미역"),
     NMarker(position: NLatLng(36.12802335, 128.3331997), id: "농협"),
     NMarker(position: NLatLng(36.14317057, 128.3943957), id: "금오공대종점"),
-    NMarker(position: NLatLng(36.13948442, 128.3967393), id: "금오공대입구(옥계중학교방면)")
+    NMarker(position: NLatLng(36.13948442, 128.3967393), id: "금오공대입구(옥계중학교방면)"),
+    NMarker(position: NLatLng(36.12252942, 128.3510414), id: "종합버스터미널"),
   ];
   late final busStopW;
   
@@ -41,7 +46,8 @@ class _BusInfoScreenState extends State<BusInfoScreen> with TickerProviderStateM
     BusSt(code:"GMB80", id:10080,subText:'경상북도 구미시 구미중앙로 70',  mainText:'구미역'),
     BusSt(code:"GMB167",id:10167,subText:'경상북도 구미시 원평동 1008-40',mainText:'농협'),
     BusSt(code:"GMB132",id:10132,subText:'경상북도 구미시 거의동 550',    mainText:'금오공대종점'),
-    BusSt(code:"GMB131",id:10131,subText:'경상북도 구미시 거의동 589-8',  mainText:'금오공대입구(옥계중학교방면)')
+    BusSt(code:"GMB131",id:10131,subText:'경상북도 구미시 거의동 589-8',  mainText:'금오공대입구(옥계중학교방면)'),
+    BusSt(code: "GMB91",id:10091,subText: "경상북도 구미시 원평동 1103",  mainText: '종합버스터미널'),
   ];
   late int curBusStop = 0;
   late int numOfBus = 0;
@@ -49,74 +55,96 @@ class _BusInfoScreenState extends State<BusInfoScreen> with TickerProviderStateM
   
   // 위치 이동 버튼과 상태
   late List<OutlineCircleButton> buttons;
-  late int curButton = 2;
+  late int curButton = 0;
 
   // 버스정류장 담당 애니메이션
   late AnimationController busStAnicon = AnimationController(duration: const Duration(milliseconds: 250), vsync: this);
   late CurvedAnimation busStCurveAni =   CurvedAnimation(parent: busStAnicon, curve: Curves.easeInOutExpo);
-  late Animation<double> busStAni =      Tween(begin: 0.0, end: 0.0).animate(busStCurveAni)..addListener(() { setState(() {}); });
+  late Animation<double> busStAni =      Tween(begin: 0.0, end: 0.0).animate(busStCurveAni)
+    ..addListener(() { setState(() {}); });
 
   // 버스리스트 담당 애니메이션
-  late Animation<double> busListAni =    Tween(begin: 0.0, end: 0.0).animate(busStCurveAni)..addListener(() { setState(() {}); });
+  late Animation<double> busListAni =    Tween(begin: 0.0, end: 0.0).animate(busStCurveAni)
+    ..addListener(() { setState(() {}); });
 
   // 두 지역(구미역, 금오공대)에 대한 화면 포지션 정의
   static const gumiPos  = NCameraPosition(target: NLatLng(36.12827222, 128.3310162), zoom: 15.5, bearing: 0, tilt: 0);
   static const gumiSPos = NCameraPosition(target: NLatLng(36.12727222, 128.3310162), zoom: 15.2, bearing: 0, tilt: 0);
   static const kumohPos = NCameraPosition(target: NLatLng(36.14132749, 128.3955675), zoom: 15.5, bearing: 0, tilt: 0);
   static const kumohSPos= NCameraPosition(target: NLatLng(36.13762749, 128.3955675), zoom: 14.0, bearing: 0, tilt: 0);
+  static const terminalPos = NCameraPosition(target: NLatLng(36.12252942, 128.3510414), zoom: 15.5, bearing: 0, tilt: 0);
+  static const terminalSPos= NCameraPosition(target: NLatLng(36.12082942, 128.3510414), zoom: 15.5, bearing: 0, tilt: 0);
   
   // 지역을 이동할 때 사용하는 것
   final cameras = [
     NCameraUpdate.scrollAndZoomTo(target: gumiPos.target,   zoom: gumiPos.zoom),  // 구미역
     NCameraUpdate.scrollAndZoomTo(target: gumiSPos.target,  zoom: gumiSPos.zoom), // 구미역 축소
     NCameraUpdate.scrollAndZoomTo(target: kumohPos.target,  zoom: kumohPos.zoom), // 금오공대
-    NCameraUpdate.scrollAndZoomTo(target: kumohSPos.target, zoom: kumohSPos.zoom) // 금오공대 축소
+    NCameraUpdate.scrollAndZoomTo(target: kumohSPos.target, zoom: kumohSPos.zoom),// 금오공대 축소
+    NCameraUpdate.scrollAndZoomTo(target: terminalPos.target,  zoom: terminalPos.zoom), // 종합터미널
+    NCameraUpdate.scrollAndZoomTo(target: terminalSPos.target, zoom: terminalSPos.zoom) // 종합터미널 축소
   ];
+  final cameraMap =  [0,2,4]; // 구미역, 금오공대, 종합터미널
+
+  // 자주 쓸 거 같은
+  NCameraAnimation myFly = NCameraAnimation.fly;
+  Duration myDuration = Duration(milliseconds: 200);
 
   @override
   void initState() {
     super.initState();
 
     // 반복문으로 줄일 수 있을 것 같음!! - 나중에
-    NCameraAnimation btnFly = NCameraAnimation.fly;
-    Duration btnDuration = Duration(milliseconds: 500);
     buttons = [
-      OutlineCircleButton( // [정류장 -> 구미역 or 금오공대] - 미완
-        child: Icon(Icons.directions_bus_filled_outlined, color: Colors.white,), radius: 50.0, borderSize: 0.5,
-        foregroundColor: Color(0xff05d686),borderColor: Colors.white,
-        onTap: () async {
-          // setState(() { curButton = 0; });
-          // gumiMCamera.setAnimation(animation: NCameraAnimation.fly, duration: Duration(milliseconds: 500));
-          // await con.updateCamera(gumiMCamera);
-          // busStopMarks[0].performClick();
-        }
-      ),
-      OutlineCircleButton( // 금오공대 -> 구미역
-        child: Icon(Icons.directions_bus_filled_outlined, color: Colors.white,), radius: 50.0, borderSize: 0.5,
-        foregroundColor: Color(0xff05d686), borderColor: Colors.white,
-        onTap: () async {
-          if (busStAnicon.isDismissed){
-            cameras[0].setAnimation(animation: btnFly, duration: btnDuration); con.updateCamera(cameras[0]);
-          } else {
-            cameras[1].setAnimation(animation: btnFly, duration: btnDuration); con.updateCamera(cameras[1]);
-          }
-          setState(() { curButton = 2; });
-          await busStopMarks[0].performClick();
-        }
-      ),
       OutlineCircleButton( // 구미역 -> 금오공대
         child: Icon(Icons.school_outlined, color: Colors.white,), radius: 50.0, borderSize: 0.5,
         foregroundColor: Color(0xff05d686),borderColor: Colors.white,
         onTap: () async {
+          final nextBusSt = 1;
           if (busStAnicon.isDismissed){
-            cameras[2].setAnimation(animation: btnFly, duration: btnDuration); con.updateCamera(cameras[2]);
+            cameras[cameraMap[nextBusSt]].setAnimation(animation: myFly, duration: myDuration); // 사실 cameraMap[0] 대신에 0 넣으면 되는데 보기 편하라고
+            await con.updateCamera(cameras[cameraMap[nextBusSt]]);
           } else {
-            cameras[3].setAnimation(animation: btnFly, duration: btnDuration); con.updateCamera(cameras[3]);
+            cameras[cameraMap[nextBusSt]+1].setAnimation(animation: myFly, duration: myDuration);
+            await con.updateCamera(cameras[cameraMap[nextBusSt]+1]);
           }
           setState(() { curButton = 1; });
           await busStopMarks[2].performClick();
         },
-      )
+      ),
+      OutlineCircleButton( // 금오공대 -> 종합터미널
+        child: Icon(Icons.directions_bus_filled_outlined, color: Colors.white,), radius: 50.0, borderSize: 0.5,
+        foregroundColor: Color(0xff05d686), borderColor: Colors.white,
+        onTap: () async {
+          final nextBusSt = 2;
+          if (busStAnicon.isDismissed){
+            cameras[cameraMap[nextBusSt]].setAnimation(animation: myFly, duration: myDuration);
+            await con.updateCamera(cameras[cameraMap[nextBusSt]]);
+          } else {
+            cameras[cameraMap[nextBusSt]+1].setAnimation(animation: myFly, duration: myDuration);
+            await con.updateCamera(cameras[cameraMap[nextBusSt]+1]);
+          }
+          setState(() { curButton = 2; });
+          await busStopMarks[4].performClick();
+        }
+      ),
+      OutlineCircleButton( // 종합터미널 -> 구미역
+          child: Icon(Icons.tram_outlined, color: Colors.white,), radius: 50.0, borderSize: 0.5,
+          foregroundColor: Color(0xff05d686),borderColor: Colors.white,
+          onTap: () async {
+            final nextBusSt = 0;
+            if (busStAnicon.isDismissed){
+              cameras[cameraMap[nextBusSt]].setAnimation(animation: myFly, duration: myDuration);
+              await con.updateCamera(cameras[cameraMap[nextBusSt]]);
+            } else {
+              cameras[cameraMap[nextBusSt]+1].setAnimation(animation: myFly, duration: myDuration);
+              await con.updateCamera(cameras[cameraMap[nextBusSt]+1]);
+            }
+            setState(() { curButton = 0; });
+            await busStopMarks[0].performClick();
+          }
+      ),
+
     ];
 
     busStopW = [
@@ -124,6 +152,7 @@ class _BusInfoScreenState extends State<BusInfoScreen> with TickerProviderStateM
       NInfoWindow.onMarker(id: busStopMarks[1].info.id, text: busStopInfos[1].mainText),
       NInfoWindow.onMarker(id: busStopMarks[2].info.id, text: busStopInfos[2].mainText),
       NInfoWindow.onMarker(id: busStopMarks[3].info.id, text: busStopInfos[3].mainText),
+      NInfoWindow.onMarker(id: busStopMarks[4].info.id, text: busStopInfos[4].mainText),
     ];
 
   }
@@ -133,8 +162,10 @@ class _BusInfoScreenState extends State<BusInfoScreen> with TickerProviderStateM
 
     // 기기의 화면 크기를 이용해 애니메이션 재설정
     double screenHeight = MediaQuery.of(context).size.height * 0.78;
-    busStAni = Tween(begin: 0.0, end: screenHeight).animate(busStCurveAni)..addListener(() {setState(() {});});
-    busListAni = Tween(begin: -MediaQuery.of(context).size.height / 2, end: 0.0).animate(busStCurveAni)..addListener(() {setState(() {});});
+    busStAni = Tween(begin: 0.0, end: screenHeight).animate(busStCurveAni)
+      ..addListener(() {setState(() {});});
+    busListAni = Tween(begin: -MediaQuery.of(context).size.height / 2, end: 0.0).animate(busStCurveAni)
+      ..addListener(() {setState(() {});});
 
     // 정류장의 정보 가져오는 함수
     Future<BusApiRes> fetchBusInfo(final nodeId) async {
@@ -153,27 +184,31 @@ class _BusInfoScreenState extends State<BusInfoScreen> with TickerProviderStateM
 
     // 버스정류장의 정보 알아오기
     Future<void> updateBusStop(int busStop) async {
-      setState(() { curBusStop = busStop; });
+      setState(() { curBusStop = busStop; isLoading = true; });
       await updateBusListBox();
 
-      for (int i = 0; i < 4; i++){
+      for (int i = 0; i < 5; i++){
         if (busStop != i) {
-          try { busStopW[i].close();} catch (e) {}
+          try { await busStopW[i].close();} catch (e) { }
         }
       }
+      await Future.delayed(Duration(milliseconds: 250));
+      setState(() {isLoading = false; loadingOpacity = 0.4;});
     }
 
     // 버스정류장 정보를 클릭할 때 이벤트 처리
     Future<void> busStationBoxClick() async {
       if (busStAnicon.isDismissed) {
-        cameras[(curBusStop==0 || curBusStop == 1) ? 1 : 3].setAnimation(animation: NCameraAnimation.fly, duration: Duration(milliseconds: 200));
-        await con.updateCamera(cameras[(curBusStop==0 || curBusStop == 1) ? 1 : 3]);
+        final index = (curBusStop%2)==1 ? curBusStop : curBusStop+1;
+        cameras[index].setAnimation(animation: myFly, duration: myDuration);
+        await con.updateCamera(cameras[index]);
 
         busStAnicon.forward();
       }
       else if (busStAnicon.isCompleted) {
-        cameras[(curBusStop==0 || curBusStop == 1) ? 0 : 2].setAnimation(animation: NCameraAnimation.fly, duration: Duration(milliseconds: 200));
-        await con.updateCamera(cameras[(curBusStop==0 || curBusStop == 1) ? 0 : 2]);
+        final index = (curBusStop%2)==0 ? curBusStop : curBusStop-1;
+        cameras[index].setAnimation(animation: myFly, duration: myDuration);
+        await con.updateCamera(cameras[index]);
 
         busStAnicon.reverse();
       }
@@ -198,12 +233,12 @@ class _BusInfoScreenState extends State<BusInfoScreen> with TickerProviderStateM
                 // 1.1.2.1 맵 컨트롤러 등록
                 con = controller;
                 // 1.1.2.2 각 마커를 클릭했을 때의 이벤트 지정
-                for (int i = 0; i < 4; i++){
+                for (int i = 0; i < 5; i++){
                   con.addOverlay(busStopMarks[i]);
-                  busStopMarks[i].setOnTapListener((overlay) async { await updateBusStop(i); busStopMarks[i].openInfoWindow(busStopW[i]);});
+                  busStopMarks[i].setOnTapListener((overlay) async { await updateBusStop(i); await busStopMarks[i].openInfoWindow(busStopW[i]);});
                 }
                 // 1.1.2.3 구미역 버튼 클릭
-                busStopMarks[0].performClick();
+                await busStopMarks[0].performClick();
               },
             ),
 
@@ -234,7 +269,7 @@ class _BusInfoScreenState extends State<BusInfoScreen> with TickerProviderStateM
             ),
 
             // 1.5 로딩 위젯
-            LoadingScreen(miliTime: 1000,),
+            (isLoading == true) ? LoadingScreen(limitTime: false, opacity: loadingOpacity,) : SizedBox(width: 0,),
           ],
         ),
       ),
@@ -246,7 +281,6 @@ class _BusInfoScreenState extends State<BusInfoScreen> with TickerProviderStateM
 
   @override
   void dispose() {
-    busStAnicon.dispose();
-    super.dispose();
+    busStAnicon.dispose(); super.dispose();
   }
 }
