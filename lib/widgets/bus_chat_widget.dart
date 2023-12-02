cba20942b76676c7594ee3aba6b021dab1f4b26a
Mne <42082748+Mne-pr@users.noreply.github.com>
2023-12-02 17:21:24 +0900

    [UPDATE] 그냥 코드 모양 수정

--- lib/widgets/bus_chat_widget.dart
+++ lib/widgets/bus_chat_widget.dart
@@ -1,15 +1,12 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kumoh_road/models/comment_model.dart';
-import 'package:provider/provider.dart';
 
 import '../models/user_model.dart';
-import '../providers/user_providers.dart';
 
 class BusChatWidget extends StatefulWidget {
-  final VoidCallback onScrollToTop;
   final Function(String) submitComment;
+  final VoidCallback onScrollToTop;
   final List<Comment> comments;
   final List<UserModel> users;
   final bool isLoading;
@@ -20,134 +17,135 @@ class BusChatWidget extends StatefulWidget {
     required this.isLoading,
     required this.comments,
     required this.users,
-    super.key});
+    super.key
+  });
 
   @override
   State<BusChatWidget> createState() => _BusChatWidgetState();
 }
-class _BusChatWidgetState extends State<BusChatWidget> {
-  final TextEditingController _commentController = TextEditingController();
-  final fire = FirebaseFirestore.instance;
 
+class _BusChatWidgetState extends State<BusChatWidget> {
+  final TextEditingController commentCon = TextEditingController();
   bool isNoChat = true;
 
+  // 현재 채팅창이 공백인지 아닌지
   void handleTxtChange() {
-    setState(() {
-      isNoChat = _commentController.text.isEmpty;
-    });
+    setState(() { isNoChat = commentCon.text.isEmpty;});
   }
 
   @override
   void initState() {
     super.initState();
-    _commentController.addListener(handleTxtChange);
+    commentCon.addListener(handleTxtChange);
   }
 
   @override
   Widget build(BuildContext context) {
-    if (widget.isLoading == true){
-      print('현재 채팅 로딩상태임');
+
+    if (widget.isLoading == true){ // 로딩 중
       return Container(
-        padding: EdgeInsets.all(0),
+        padding:    EdgeInsets.all(0),
         decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border(top: BorderSide(width: 0.5,color: const Color(0xFF3F51B5).withOpacity(0.2),),),
-        ),
+          color:     Colors.white,
+          border:    Border(
+            top:      BorderSide(width: 2.0, color: const Color(0xFF3F51B5).withOpacity(0.2),),
+            bottom:   BorderSide(width: 0.5, color: const Color(0xFF3F51B5).withOpacity(0.2),),
+        ),),
+
         child: Center(
           child: SizedBox(
             height: MediaQuery.of(context).size.height / 2,
-            child: Center( child: CircularProgressIndicator(),),
-          ),
-        ),
+            child:  Center( child: CircularProgressIndicator(),),
+        ),),
+
       );
     }
 
-    final userProvider = Provider.of<UserProvider>(context);
-    userProvider.startListeningToUserChanges();
     List<Comment> commentList = widget.comments;
-    List<UserModel> userList = widget.users;
+    List<UserModel> userList  = widget.users;
 
+    // 댓글 추가 로직
     void submitComment() {
-      // 메인에 댓글 내용 전달
-      widget.submitComment(_commentController.text);
-
-      // 마무리 - ui
-      _commentController.clear();
-      try {FocusScope.of(context).unfocus();} catch(e) {}
+      widget.submitComment(commentCon.text);              // 부모에 댓글 전달
+      commentCon.clear();                                 // 채팅창 클리어
+      try {FocusScope.of(context).unfocus();} catch(e) {} // 키보드 비활성화
     }
 
     return Container(
-      margin: EdgeInsets.zero,
+      margin:     EdgeInsets.zero,
       decoration: BoxDecoration(
-        border: Border(
-          top: BorderSide(width: 2.0, color: const Color(0xFF3F51B5).withOpacity(0.2),),
-          bottom: BorderSide(width: 0.5, color: const Color(0xFF3F51B5).withOpacity(0.2),),
-        ),
-      ),
+        border:    Border(
+          top:      BorderSide(width: 2.0, color: const Color(0xFF3F51B5).withOpacity(0.2),),
+          bottom:   BorderSide(width: 0.5, color: const Color(0xFF3F51B5).withOpacity(0.2),),
+      ),),
+
       child: Column(
         children: [
-          // 댓글
-          Container(
+
+          Container( // 댓글 출력 창
             decoration: BoxDecoration( color: Colors.white,),
-            height: MediaQuery.of(context).size.height / 2 - 60,
+            height:     MediaQuery.of(context).size.height / 2 - 60,
+
             child: RefreshIndicator(
               displacement: 100000, // 인디케이터 보이지 않도록
-              onRefresh: () async {widget.onScrollToTop();},
+              onRefresh:    () async {widget.onScrollToTop();},
+
               child: ListView.builder(
-                itemCount: commentList.length,
+                itemCount:   commentList.length,
                 itemBuilder: (context, index) {
                   Comment comment = commentList[index]; // 댓글 유저 수 같아야 함.. 탈퇴한 유저? 아직 처리안함
-                  UserModel user = userList[index];
-                  // 첫째 줄
-                  if (index == 0) {
+                  UserModel user  = userList[index];
+
+                  if (index == 0) { // 첫째 줄
                     return Container(
                       child: Stack(
                         children: [
                           Container( alignment: Alignment.center, height: 22.0, child: Icon(Icons.arrow_drop_down,size: 20.0,), ),
-                          ListTile(title: Text('${user.nickname} : ${comment.comment}'))
-                        ],
-                      ),
-                    );
-                  }
+                          ListTile (title: Text('${user.nickname} : ${comment.comment}')), // ui 수정해야
+                  ],),);}
 
-                  // 나머지 줄
-                  return Container(
-                    decoration: BoxDecoration( border: Border( top: BorderSide(width: 1.0, color: Colors.grey.shade200),), ),
-                    child: ListTile(title: Text('${user.nickname} : ${comment.comment}')),
-                  );
+                  else { // 나머지 줄
+                    return Container(
+                      decoration: BoxDecoration( border: Border( top: BorderSide(width: 1.0, color: Colors.grey.shade200),), ),
+                      child:      ListTile(title: Text('${user.nickname} : ${comment.comment}')), // ui 수정해야
+                  );}
+
                 },
               ),
             ),
           ),
 
-          // 댓글 입력
-          Container(
+
+          Container( // 댓글 입력 창
             decoration: BoxDecoration(color: Colors.white, border: Border(top: BorderSide(width: 1.0, color: const Color(0xFF3F51B5).withOpacity(0.2),),) ),
-            height: 60, //
+            height:     60,
+
             child: Padding(
               padding: const EdgeInsets.fromLTRB(5, 8, 5, 8),
+
               child: Row(
                 children: [
+
                   Expanded(
                     child: TextField(
-                      controller: _commentController,
-                      decoration: InputDecoration(filled: true, hintText: '댓글 입력',fillColor: const Color(0xFF3F51B5).withOpacity(0.1)),
+                      controller:  commentCon,
+                      decoration:  InputDecoration(filled: true, hintText: '댓글 입력',fillColor: const Color(0xFF3F51B5).withOpacity(0.1)),
                       onSubmitted: (String text) { if (!isNoChat) submitComment(); },
-                    ),
-                  ),
+                  ),),
+
                   SizedBox(width: 5,),
+
                   Material( // 버튼이 피드백 대처를 위한 공간 마련
                     color: Colors.transparent,
                     child: InkWell(
-                      onTap: isNoChat ? null : submitComment,
+                      onTap:        isNoChat ? null : submitComment,
                       borderRadius: BorderRadius.circular(24), // 클릭 피드백 동그라미
-                      splashColor: Color(0xff05d686), // 물결 효과 색상 설정
+                      splashColor:  Color(0xff05d686), // 물결 효과 색상 설정
                       child: Padding(
                         padding: EdgeInsets.all(9.0),
-                        child: Icon(Icons.send, color: isNoChat ? Colors.grey : const Color(0xFF3F51B5)),
-                      ),
-                    ),
-                  ),
+                        child:   Icon(Icons.send, color: isNoChat ? Colors.grey : const Color(0xFF3F51B5)),
+                  ),),),
+
                 ],
               ),
             ),
@@ -159,7 +157,7 @@ class _BusChatWidgetState extends State<BusChatWidget> {
 
   @override
   void dispose() {
-    _commentController.dispose();
+    commentCon.dispose();
     super.dispose();
   }
 }
