71fbead958f20afc15632cf3db81dc7a2e6b0a16
thk <qkdrhks123@gmail.com>
2023-12-13 01:04:37 +0900

    [FIX] 최초 실행시 나이와 성별 입력 불가능 오류 해결

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:kumoh_road/providers/user_providers.dart';
 import 'package:kumoh_road/screens/launch_screens/intro_screen.dart';
 import 'package:kumoh_road/screens/main_screens/main_screen.dart';
 import 'package:kumoh_road/utilities/material_color_utile.dart';
+import 'package:numberpicker/numberpicker.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
@@ -46,6 +47,11 @@ class KumohRoad extends StatelessWidget {
                 _showAccountSuspendedDialog(context, userProvider);
               });
             }
+            if (userProvider.age == null || userProvider.gender == null) {
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                _showAdditionalInfoDialog(context, userProvider);
+              });
+            }
             return MainScreen();
           } else {
             return IntroScreen();
@@ -80,4 +86,85 @@ class KumohRoad extends StatelessWidget {
       },
     );
   }
+
+  void _showAdditionalInfoDialog(BuildContext context, UserProvider userProvider){
+    int age = 25; // 초기 나이 설정
+    String gender = '남성'; // 기본값 설정
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: const Text('추가 정보 입력'),
+              contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              content: Container(
+                width: double.maxFinite,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      "이 단계는 향후 비즈니스 검증 절차를 완료한 후\n 카카오 로그인을 통해 자동으로 수집될 예정입니다.",
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text('나이'),
+                    Center(
+                      child: SizedBox(
+                        height: 100,
+                        child: NumberPicker(
+                          value: age,
+                          minValue: 19,
+                          maxValue: 30,
+                          onChanged: (value) => setState(() => age = value),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text('성별'),
+                    Center(
+                      child: Container(
+                        width: 150,
+                        child: DropdownButton<String>(
+                          isExpanded: true,
+                          value: gender,
+                          items: <String>['남성', '여성'].map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              gender = newValue!;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('저장'),
+                  onPressed: () async {
+                    userProvider.updateUserInfo(age: age, gender: gender);
+                    Navigator.pop(context);
+                    Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) => MainScreen()),
+                          (Route<dynamic> route) => false,
+                    ); //
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
 }
